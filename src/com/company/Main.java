494f98f50376209a4927d96d7f3aeea414673250
Muhammed <muhammedtoichubai@gmail.com>
2022-02-24 23:53:37 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)  {
+//        Try with resources-те колдоно ала турган Car деген класс тузунуз
+//        close() деген метод "Машина жабылып жатат" деп консольго чыгаруусу керек.
+//        try unchecked exception дарды кармап аларды игнор кылыш керек.
+//                main методдо try блоктун ичинде drive() деген методду чакырыныз,
+//                ал консольго "Машина журуп жатат" деп чыгаруусу керек
+//        Эскертуу: маселени чыгарууда try with resources колдонуу керек.
+        //try (Car car = new Car()) {
+
+        // public class Car extends AutoClosalbe
+//    }
+        try (Car car = new Car()) {
+        car.drive();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
