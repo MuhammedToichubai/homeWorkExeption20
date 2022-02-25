9b72797507ceb044b42478db379342590d9d7984
Muhammed <muhammedtoichubai@gmail.com>
2022-02-25 03:38:25 -0600

    carPolo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args)throws IOException  {
 //        Try with resources-те колдоно ала турган Car деген класс тузунуз
 //        close() деген метод "Машина жабылып жатат" деп консольго чыгаруусу керек.
 //        try unchecked exception дарды кармап аларды игнор кылыш керек.
@@ -12,11 +14,12 @@ public class Main {
 //                ал консольго "Машина журуп жатат" деп чыгаруусу керек
 //        Эскертуу: маселени чыгарууда try with resources колдонуу керек.
 
-        try (Car car = new Car()) {
+        try (Car car = new Car("Polo","Wolksvagen","White",1.8,
+                                        LocalDate.of(2014, Month.AUGUST,11))) {
+            System.out.println(car);
         car.drive();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-
     }
 }
